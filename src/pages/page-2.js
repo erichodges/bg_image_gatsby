d9457f21b6db59335218ba5b1ee4bda21fa3a5bf
erichodges <14981329+erichodges@users.noreply.github.com>
2019-09-25 16:13:01 -0700

    Text edit: Page 2 text update

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import { Link } from 'gatsby';
+import React from 'react';
+import Image from '../components/image';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import Image from '../components/image'
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>page 2</h1>
     <p>Welcome to page 2</p>
     <div style={{ maxWidth: `300px`, margin: `0 auto`, marginBottom: `1.45rem` }}>
       <Image />
